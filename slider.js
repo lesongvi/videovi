7fd414f470a440fc1d5a8ce6a382c0727c72609e
Lê Song Vĩ <rappervn88@gmail.com>
2018-03-29 16:27:42 +0700

    Create slider.js

---
+++ slider.js
@@ -0,0 +1,19 @@
+
+
+		logo_image_size = "819" ;
+
+		logo_image_xpos = "" ;
+
+		logo_image_ypos = "" ;
+
+		page_top_margin = "0" ;
+
+        page_top_padding = "0" ;
+
+        app_bottom_margin = "15" ;
+
+        page_transparency = "55" ;
+
+		background_rgb_dec = "0,34,60" ;
+
+
