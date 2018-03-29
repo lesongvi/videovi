d2c2b9cd80e318b768a6a27d0659d471ed8e8e2f
Lê Song Vĩ <rappervn88@gmail.com>
2018-03-29 16:42:53 +0700

    Create idsc.js

---
+++ idsc.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){ new jPlayerPlaylist({ jPlayer: "#jquery_jplayer_1fpl_id",cssSelectorAncestor: "#jp_container_1fpl_id"}, [{
+							title:"On My Way Home",
+							album:"Sitewide Audio Player",
+							mp3:"https://memberdata.s3.amazonaws.com/pt/ptx2013/audio/ptx2013_audio_all_500115094.mp3?q=126",
+							song_id: "2686136155",
+							poster: "https://memberdata.s3.amazonaws.com/pt/ptx2013/photos/ptx2013_photo_gal_33347_photo_1757629399_sm.jpg",
+							itunes: "https://itunes.apple.com/us/album/on-my-way-home/id907175342?i=907175410&uo=4",
+							amazon: "http://www.amazon.com/gp/product/B00MI6J8UM/ref=dm_ws_tlw_trk2?ie=UTF8&qid=1416930273&sr=1-1",
+							cd_baby: ""
+							},{
+							title:"Problem",
+							album:"Sitewide Audio Player",
+							mp3:"https://memberdata.s3.amazonaws.com/pt/ptx2013/audio/ptx2013_audio_all_2106667927.mp3?q=814",
+							song_id: "2686136153",
+							poster: "https://memberdata.s3.amazonaws.com/pt/ptx2013/photos/ptx2013_photo_gal_33347_photo_1757629399_sm.jpg",
+							itunes: "https://itunes.apple.com/us/album/problem/id907175342?i=907175367&uo=4",
+							amazon: "http://www.amazon.com/gp/product/B00MI6J8DE/ref=dm_ws_tlw_trk1?ie=UTF8&qid=1416930273&sr=1-1",
+							cd_baby: ""
+							},{
+							title:"La La Latch",
+							album:"Sitewide Audio Player",
+							mp3:"https://memberdata.s3.amazonaws.com/pt/ptx2013/audio/ptx2013_audio_all_1099994367.mp3?q=378",
+							song_id: "2686136154",
+							poster: "https://memberdata.s3.amazonaws.com/pt/ptx2013/photos/ptx2013_photo_gal_33347_photo_1757629399_sm.jpg",
+							itunes: "https://itunes.apple.com/us/album/la-la-latch/id907175342?i=907175413&uo=4",
+							amazon: "http://www.amazon.com/gp/product/B00MI6J9OC/ref=dm_ws_tlw_trk3?ie=UTF8&qid=1416930273&sr=1-1",
+							cd_baby: ""
+							},{
+							title:"See Through",
+							album:"Sitewide Audio Player",
+							mp3:"https://memberdata.s3.amazonaws.com/pt/ptx2013/audio/ptx2013_audio_all_2055056173.mp3?q=348",
+							song_id: "2686136157",
+							poster: "https://memberdata.s3.amazonaws.com/pt/ptx2013/photos/ptx2013_photo_gal_33347_photo_1757629399_sm.jpg",
+							itunes: "https://itunes.apple.com/us/album/see-through/id907175342?i=907175417&uo=4",
+							amazon: "http://www.amazon.com/gp/product/B00MI6JBXG/ref=dm_ws_tlw_trk5?ie=UTF8&qid=1416930273&sr=1-1",
+							cd_baby: ""
+							},{
+							title:"Papaoutai",
+							album:"Sitewide Audio Player",
+							mp3:"https://memberdata.s3.amazonaws.com/pt/ptx2013/audio/ptx2013_audio_all_2069412042.mp3?q=205",
+							song_id: "2686136158",
+							poster: "https://memberdata.s3.amazonaws.com/pt/ptx2013/photos/ptx2013_photo_gal_33347_photo_1757629399_sm.jpg",
+							itunes: "https://itunes.apple.com/us/album/papaoutai/id907175342?i=907175419&uo=4",
+							amazon: "http://www.amazon.com/gp/product/B00MI6JDF2/ref=dm_ws_tlw_trk6?ie=UTF8&qid=1416930273&sr=1-1",
+							cd_baby: ""
+							}], {top_frame_q: "0",playlist_q: "0",swfPath: "/js/s101_admin/mp3_player/js/",supplied: "mp3",wmode: "window",loop:false,main_player: "main",jplayer_id: "fpl_id",s_netw: "0",s_commerce: "0",playlistOptions: {autoPlay: false}});});
